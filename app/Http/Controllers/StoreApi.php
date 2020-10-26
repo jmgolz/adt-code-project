a35c516a2b73e1ef5af81b04db7ceb1b3e9fdca5
James Golz <jmgolz@gmail.com>
2020-10-26 17:27:32 -0400

    change return complete

--- app/Http/Controllers/StoreApi.php
+++ app/Http/Controllers/StoreApi.php
@@ -8,20 +8,34 @@ use Illuminate\Http\Request;
 class StoreApi extends Controller
 {
     private $dollarDenominations = array(100, 50, 20, 10, 5, 1);
+    private $centDenominations = array(25, 10, 5, 1);
 
 
     public function getChangeOwed(Request $request)
     {
-        $amountOwed = 2337.35;
+        $amountOwed = explode('.', 2337.37);
         $amountOwedByDenomination = array();
 
+        // Calculate dollar amounts
         for ($i = 0; $i < count($this->dollarDenominations); $i++) {
-            if ($amountOwed >= $this->dollarDenominations[$i]) {
-                $tempAmount = intval($amountOwed / $this->dollarDenominations[$i]);
+            if ($amountOwed[0] >= $this->dollarDenominations[$i]) {
+                $tempAmount = intval($amountOwed[0] / $this->dollarDenominations[$i]);
 
                 $amountOwedByDenomination['dollars'][$this->dollarDenominations[$i]] = $tempAmount;
-                $amountOwed = $amountOwed - ($amountOwedByDenomination['dollars'][$this->dollarDenominations[$i]] * $this->dollarDenominations[$i]);
+                $amountOwed[0] = $amountOwed[0] - ($amountOwedByDenomination['dollars'][$this->dollarDenominations[$i]] * $this->dollarDenominations[$i]);
             }
+        }
+
+        // Calculate cents if applicable.
+        if (count($amountOwed) > 1) {
+            for ($i = 0; $i < count($this->centDenominations); $i++) {
+                if ($amountOwed[1] >= $this->centDenominations[$i]) {
+                    $tempAmount = intval($amountOwed[1] / $this->centDenominations[$i]);
+    
+                    $amountOwedByDenomination['cents'][$this->centDenominations[$i]] = $tempAmount;
+                    $amountOwed[1] = $amountOwed[1] - ($amountOwedByDenomination['cents'][$this->centDenominations[$i]] * $this->centDenominations[$i]);
+                }
+            }   
         }
 
         return response()->json($amountOwedByDenomination);
